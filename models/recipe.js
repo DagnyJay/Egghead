13892f5c599e122fa60ac9909351d0480acf0db6
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-03-05 16:41:57 -0700

    implement grid on show page

--- models/recipe.js
+++ models/recipe.js
@@ -14,7 +14,7 @@ const recipeSchema = new Schema({
     keyIngredient: {type: String, required: false},
     ingredients: {type: String, required: false},
     instructions: {type: String, required: false},
-    img: {data: Buffer, contentType: String, required: false},
+    img: {type: String, required: false},
     servings: {type: Number, required: false, min: 1, max: 20},
     prepTime: {type: String, required: false},
     cookTime: {type: String, required: false},
