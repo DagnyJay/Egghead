4d4e4623d35f95135385afc6f3647313da867396
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-03-06 08:29:04 -0700

    improve UI

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,14 +3,19 @@ var router = express.Router();
 const recipesCtrl = require('../controllers/recipes');
 
 router.get('/', recipesCtrl.index);
-router.get('/new', recipesCtrl.new);
+router.get('/new', isLoggedIn, recipesCtrl.new);
 router.get('/:id', recipesCtrl.show);
-router.get('/:id/edit', recipesCtrl.edit);
+router.get('/:id/edit', isLoggedIn, recipesCtrl.edit);
 
-router.post('/', recipesCtrl.create);
+router.post('/', isLoggedIn, recipesCtrl.create);
 
-router.put('/:id', recipesCtrl.update);
+router.put('/:id', isLoggedIn, recipesCtrl.update);
 
-router.delete('/:id', recipesCtrl.delete);
+router.delete('/:id', isLoggedIn, recipesCtrl.delete);
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+  }
 
 module.exports = router;
