4b976b58735a0a14ce6fdc4bc29ae0f3dd4e34a1
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-03-10 14:24:48 -0600

    add oauth for comments route

--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
-router.post('/recipes/:id/comments', commentsCtrl.create);
+router.post('/recipes/:id/comments', isLoggedIn, commentsCtrl.create);
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
